da06d656cfe6ec19cc9e66d031cf3629f2be7f10
Thitat Auareesuksakul <flux@thitat.net>
2025-03-27 14:02:46 +0900

    Show cursorline and fix colors

--- init.lua
+++ init.lua
@@ -67,8 +67,9 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
--- Don't show which line your cursor is on
-vim.opt.cursorline = false
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = 'number'
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
@@ -878,6 +879,7 @@ require('lazy').setup({
       invert_signs = true,
       overrides = {
         CopilotSuggestion = { link = 'GruvboxFg4' },
+        CursorLineSign = { link = 'CursorLineNr' },
       },
     },
     init = function()
