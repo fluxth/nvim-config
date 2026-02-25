5ddad938d81570fac3e3efc34ace349a944ec610
Thitat Auareesuksakul <flux@thitat.net>
2026-02-25 09:17:29 +0000

    [PATCH 05] add custom plugins

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,39 @@
+vim.pack.add {
+  {
+    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
+    version = vim.version.range '3',
+  },
+  -- dependencies
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/MunifTanjim/nui.nvim',
+  'https://github.com/nvim-mini/mini.nvim', -- icons
+}
+
+require('neo-tree').setup {
+  close_if_last_window = true,
+  window = {
+    position = 'left',
+    width = 36,
+    mappings = {
+      ['l'] = 'open',
+      ['h'] = 'close_node',
+    },
+  },
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_by_name = {
+        '.git',
+      },
+    },
+    follow_current_file = {
+      enabled = true,
+    },
+  },
+}
+
+vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<CR>', { desc = 'NeoTree toggle' })
+vim.keymap.set('v', '<leader>e', '<cmd>Neotree toggle<CR>', { desc = 'NeoTree toggle' })
+
+vim.keymap.set('n', '<leader>b', '<cmd>Neotree toggle buffers<CR>', { desc = 'NeoTree buffers toggle' })
+vim.keymap.set('v', '<leader>b', '<cmd>Neotree toggle buffers<CR>', { desc = 'NeoTree buffers toggle' })
