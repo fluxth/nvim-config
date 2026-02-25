b103267683d22287502c351d2c3b95734048b5e3
Thitat Auareesuksakul <flux@thitat.net>
2026-02-25 09:17:29 +0000

    feat: add custom plugins

---
+++ lua/custom/plugins/mason-lock.lua
@@ -0,0 +1,9 @@
+return {
+  'zapling/mason-lock.nvim',
+  dependencies = { 'williamboman/mason.nvim' },
+  init = function()
+    require('mason-lock').setup {
+      lockfile_path = vim.fn.stdpath 'config' .. '/mason-lock.json',
+    }
+  end,
+}
